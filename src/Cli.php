e0cc7873d27d894ab24d8d08c0ac0374a47c2396
fedro <bfd77@yandex.ru>
2022-02-02 23:16:24 +0300

    fix brain-games

--- src/Cli.php
+++ src/Cli.php
@@ -5,6 +5,4 @@ namespace Brain\Games;
 use function cli\line;
 use function cli\prompt;
 
-line('Welcome to the Brain Game!');
-$name = prompt('May I have your name?');
-line("Hello, %s!", $name);
+line('Welcome to the Brain Games!');
